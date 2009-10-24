7599a46644cff0b6286e3156bb5787380d9e932a
reckase <reckase@b551d8ae-1eaf-49cd-84df-210f286344ea>
2009-10-24 13:11:01 +0000

    added function prototype for new palette interp
    git-svn-id: http://flam3.svn.sourceforge.net/svnroot/flam3/branches/early-clip/src@847 b551d8ae-1eaf-49cd-84df-210f286344ea

--- parser.h
+++ parser.h
@@ -31,4 +31,5 @@ int parse_flame_element(xmlNode *flame_node, flam3_genome *loc_current_cp);
 int parse_xform_xml(xmlNode *chld_node,flam3_xform *this_xform, int *num_xaos, 
                     flam3_chaos_entry **xaos, int numstd, int motionxf);
 void flam3_edit_print(FILE *f, xmlNodePtr editNode, int tabs, int formatting);
+int flam3_interp_missing_colors(flam3_genome *cp);
 #endif
